cc22bf69859de186fa8c5a801feb32d72ce540f6
Samin <samin.mhr2004@gmail.com>
2026-09-20 13:49:57 +0330

    Concept: Browser Rendering - Ended

---
+++ 01-browser-rendering/solution/script.js
@@ -0,0 +1,14 @@
+const box = document.querySelector("#box");
+const button = document.querySelector("#moveButton");
+
+let position = 0;
+
+button.addEventListener("click", () => {
+  for (let i = 0; i < 500; i++) {
+    position += 1;
+  }
+
+  box.style.width = position + "px";
+
+  console.log(box.offsetWidth);
+});
